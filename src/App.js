cff5737c0a1d762b078b1495821e1d0a8bd04db3
Sagartare12 <sagartare2014@gmail.com>
2021-05-13 01:59:52 +0530

    Create public and private route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
-
-
+import 'rsuite/dist/styles/rsuite-default.css'
+import './styles/main.scss'
+import { Switch , Route } from 'react-router-dom'
+import SignIn from './pages/SignIn'
+import PrivateRoute from './components/PrivateRoute'
+import Home from './pages/Home'
+import PublicRoute from './components/PublicRoute'
 function App() {
   return (
-    <div className="App">
-      hi
-      </div>
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
   );
 }
 
