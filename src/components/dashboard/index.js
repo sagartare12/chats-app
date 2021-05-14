03e42f927069bd9157b01c6ded061ad5bff6e16c
Sagartare12 <sagartare2014@gmail.com>
2021-05-14 20:53:18 +0530

    Create editable and reusable component

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,27 +1,38 @@
 import React from 'react'
-import { Button, Drawer } from 'rsuite'
+import { Button, Drawer ,Divider } from 'rsuite'
 import { useProfile } from '../../context/profileContext'
+import { EditableInput } from './EditableInput';
 
  const Dashboard = ({ onSignOut }) => {
 
     const { profile } = useProfile();
+
+    const onSave = async newData => {
+        console.log('newData' , newData);
+    };
     return (
         <>
-            <Drawer.Header>
-                <Drawer.Title>
-                    Dashboard
-                </Drawer.Title>
-            </Drawer.Header>
-        <Drawer.Body>
-            <h3>Hey, {profile.name}</h3>
+                    <Drawer.Header>
+                        <Drawer.Title>
+                            Dashboard
+                        </Drawer.Title>
+                    </Drawer.Header>
+                <Drawer.Body>
+                    <h3>Hey, {profile.name}</h3>
+                    <Divider />
+                    <EditableInput 
+                    name="nickname"
+                    initialValue={profile.name}
+                    onSave={onSave}
+                    label={<h6 className="mb-2">Nickname</h6>}
+                    />
+                </Drawer.Body>
 
-        </Drawer.Body>
-
-        <Drawer.Footer>
-            <Button block color="red" onClick={onSignOut}>
-                Sign Out
-            </Button>
-        </Drawer.Footer>
+                <Drawer.Footer>
+                    <Button block color="red" onClick={onSignOut}>
+                        Sign Out
+                    </Button>
+                </Drawer.Footer>
         </>
     )
 }
