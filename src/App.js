4416f87b9b9fed090275bb2c2a5ca4864f0b4c73
Sagartare12 <sagartare2014@gmail.com>
2021-05-14 12:29:03 +0530

    Manage user profile using context

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import SignIn from './pages/SignIn'
 import PrivateRoute from './components/PrivateRoute'
 import Home from './pages/Home'
 import PublicRoute from './components/PublicRoute'
+import { ProfileProvider } from './context/profileContext'
 function App() {
   return (
-    <Switch>
-    <PublicRoute path="/signin">
-      <SignIn />
-    </PublicRoute>
-    <PrivateRoute path="/">
-      <Home />
-    </PrivateRoute>
-    </Switch>
-     
+    <ProfileProvider>
+      <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+      </Switch>
+     </ProfileProvider>
   );
 }
 
--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import { Redirect , Route  } from 'react-router';
-
+import { useProfile } from '../context/profileContext';
+import {Container , Loader} from 'rsuite'
  const PublicRoute = ( { children , ...routeProps }) => {
 
-    const profile = false;
+    const { isLoading , profile } = useProfile();
+
+    if(isLoading && !profile){
+        return <Container>
+            <Loader center vertical size="md" content="Loading" speed="slow" />
+        </Container>
+    }
+
+    if(profile && !isLoading){
+        return <Redirect to="/signin" />;
+    }
     if(profile){
     return <Redirect to="/" />
 }
