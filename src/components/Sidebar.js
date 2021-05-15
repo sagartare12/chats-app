0b561336954434c3d853b3dfe33a7721226846a8
Sagartare12 <sagartare2014@gmail.com>
2021-05-15 16:23:34 +0530

    Fix the bug in createRoomBtnModel page , is related to the input

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { CreateRoomBtnModal } from './dashboard/CreateRoomBtnModal'
 import DashboardToggle from './dashboard/DashboardToggle'
  const Sidebar = () => {
     return (
         <div className="h-100 pt-2">
             <div>
                 <DashboardToggle />
+                <CreateRoomBtnModal />
             </div>
         </div>
     )
